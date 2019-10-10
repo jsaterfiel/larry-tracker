3a17f6d4e6b48026962670edfb2b9ef7e5d660fd
Tony Hawkson <123248001@qq.com>
2019-10-10 16:45:02 -0400

    AddUser

--- ui-admin/src/AddUser.js
+++ ui-admin/src/AddUser.js
@@ -1,14 +1,72 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
+import { Form, Button, Alert, InputGroup, FormControl, Modal } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Login.css';
+import ConfirmModal from './ConfirmModal';
 
 class AddUser extends Component {
-  componentWillUpdate(nextProps) {
+
+
+  constructor(props) {
+    super(props);
   }
 
   render() {
+
     return (
-      <div className="AddUserPage">
-        AddUser page
-      </div>
+        <>
+          <Form>
+            <InputGroup className="mb-3">
+              <InputGroup.Prepend>
+                <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                  placeholder="Username"
+                  aria-label="Username"
+                  aria-describedby="basic-addon1"
+              />
+            </InputGroup>
+
+            <InputGroup className="mb-3">
+              <FormControl
+                  placeholder="Email"
+                  aria-label="Email"
+                  aria-describedby="basic-addon2"
+              />
+              <InputGroup.Append>
+                <InputGroup.Text id="basic-addon2">@example.com</InputGroup.Text>
+              </InputGroup.Append>
+            </InputGroup>
+
+            <label htmlFor="basic-url">Your vanity URL</label>
+            <InputGroup className="mb-3">
+              <InputGroup.Prepend>
+                <InputGroup.Text id="basic-addon3">
+                  https://example.com/users/
+                </InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl id="basic-url" aria-describedby="basic-addon3" />
+            </InputGroup>
+
+            <InputGroup className="mb-3">
+              <InputGroup.Prepend>
+                <InputGroup.Text>$</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl aria-label="Amount (to the nearest dollar)" />
+              <InputGroup.Append>
+                <InputGroup.Text>.00</InputGroup.Text>
+              </InputGroup.Append>
+            </InputGroup>
+
+            <InputGroup>
+              <InputGroup.Prepend>
+                <InputGroup.Text>Comment</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl as="textarea" aria-label="Comment" />
+            </InputGroup>
+          </Form>
+          <ConfirmModal/>
+        </>
     );
   }
 }
