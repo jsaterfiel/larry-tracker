6b2785710e507608b45097a93972f48857374c6b
John Saterfiel <john.saterfiel@oracle.com>
2019-10-12 15:44:58 -0400

    (saterfiel) adding swagger docs

--- api/src/lookup.js
+++ api/src/lookup.js
@@ -8,21 +8,13 @@ const DB_ESC_REG = /['\\]/g;
  * Helps with performing lookups for client data
  */
 const api = {
-  /**
-   * lookup.getClients
-   * returns the list of clients orderd by name ascending
-   */
+
   getClients: async () => {
-    const query = 'select client_code from `default`.byClientLevels group by client_code order by client_code asc;';
+    const query = 'select client_code as "clientCode" from `default`.byClientLevels group by client_code order by client_code asc;';
 
     return await db.query(query).toPromise();
   },
 
-  /**
-   * lookup.getLevels
-   * returns the levels for a client_code
-   * @param client String (required)
-   */
   getLevels: async (client) => {
     if (client && typeof client == 'string') {
       client = client.replace(DB_ESC_REG, '');
@@ -30,7 +22,7 @@ const api = {
       throw new Error('pcode required');
     }
 
-    const query = `SELECT level1_id
+    const query = `SELECT level1_id as "level1ID"
     FROM \`default\`.byClientLevels
     WHERE client_code = '${client}'
     group by client_code, level1_id
